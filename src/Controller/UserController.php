1ffb3c761653aa48f44e6666aab76d56d14828a7
karapuce <typhaine.demangeon@gmail.com>
2020-04-15 18:22:43 +0200

    création du manager controller et de la vue User

---
+++ src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\UserManager;
+
+class UserController extends AbstractController
+{
+    /**
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        $userManager = new UserManager();
+        $users = $userManager->selectAll();
+
+        return $this->twig->render('User/index.html.twig', ['users' => $users]);
+    }
+}
