ea12d84b3a4403bb015b51fdd6959c1c5fe753ca
karapuce <typhaine.demangeon@gmail.com>
2020-04-16 18:22:07 +0200

    redirection pour le login + regex pour l'inscription et verification avant creation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -24,16 +24,28 @@ class UserController extends AbstractController
     {
         $message="";
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
+            $patternTel= '/^0[1-689][0-9]{8}$/';
+            $patternPass='/^\S*(?=\S{6,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/' ;
+            if (empty($_POST['firstname']) && empty($_POST['lastname'])) {
+                $message= "Veuillez remplir les champs NOM et PRENOM s'il vous plaît";
+                return $this->twig->render('User/add.html.twig', ['message'=>$message]);
+            } elseif (!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
                 $message= "L'adresse mail est invalide";
+                return $this->twig->render('User/add.html.twig', ['message'=>$message]);
+            } elseif (preg_match($patternTel, $_POST['tel']) === 0) {
+                $message= "Il y a un problème avec le numéro de téléphone.";
+                return $this->twig->render('User/add.html.twig', ['message'=>$message]);
+            } elseif (preg_match($patternPass, $_POST['password']) === 0) {
+                $message= "Votre mot de passe doit faire minimum 6 caractères 
+                et contenir au moins une majuscule et un chiffre";
                 return $this->twig->render('User/add.html.twig', ['message'=>$message]);
             } else {
                 $userManager = new UserManager();
                 $user = [
-                    'firstname' => $_POST['firstname'],
-                    'lastname' => $_POST['lastname'],
+                    'firstname' => ucfirst($_POST['firstname']),
+                    'lastname' => strtoupper($_POST['lastname']),
                     'password' => $_POST['password'],
-                    'mail' => $_POST['mail'],
+                    'mail' => strtolower($_POST['mail']),
                     'tel' => $_POST['tel'],
                 ];
                 if ($userManager->checkEmail($user) === false) {
