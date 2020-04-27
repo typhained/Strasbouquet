fa5631c7e6e3cc497d1fa76c8f77d8927bd15d91
franck <franck7980@gmail.com>
2020-04-27 09:29:58 +0200

    gallerie manager update

--- src/Model/GallerieManager.php
+++ src/Model/GallerieManager.php
@@ -33,4 +33,15 @@ class GallerieManager extends AbstractManager
 
         return $statement->fetch();
     }
+    /**
+     * @param int $id
+     */
+    public function delete(int $id): void
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . "
+         INNER JOIN ".self::BOUQUET. "WHERE :bouquet.id = gallerie.id_bouquet");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+    }
 }
