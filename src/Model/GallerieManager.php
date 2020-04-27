33f3de9b664655f4ddb7ab906ae6b590d7585435
franck <franck7980@gmail.com>
2020-04-27 18:28:19 +0200

    requete sql insertbouquet

--- src/Model/GallerieManager.php
+++ src/Model/GallerieManager.php
@@ -44,4 +44,21 @@ class GallerieManager extends AbstractManager
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
     }
+    /**
+     * insert into
+     */
+    public function insertBouquet(array $gallerie): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . "(nom, file1, file2, id_bouquet)
+        VALUES (:nom, :file1, :file2, :id_bouquet)");
+        $statement->bindValue('nom', $gallerie['nom'], \PDO::PARAM_STR);
+        $statement->bindValue('file1', $gallerie['file1'], \PDO::PARAM_STR);
+        $statement->bindValue('file2', $gallerie['file2'], \PDO::PARAM_STR);
+        $statement->bindValue('id_bouquet', $gallerie['id_bouquet'], \PDO::PARAM_INT);
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
 }
