fac682cc861108d0342d983b8fd972b7b3910358
karapuce <typhaine.demangeon@gmail.com>
2020-05-05 18:15:21 +0200

    majindex

--- public/index.php
+++ public/index.php
@@ -10,6 +10,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
+if (!isset($_SESSION['role'])) {
+    $_SESSION['role'] = 'disconnect';
+}
+
 if (getenv('ENV') === false) {
     require_once __DIR__ . '/../config/debug.php';
     require_once __DIR__ . '/../config/db.php';
