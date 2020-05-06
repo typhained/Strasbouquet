5492932787ecb783245d70d1c4ec328495c31762
karapuce <typhaine.demangeon@gmail.com>
2020-05-06 09:44:29 +0200

    maj bdd

--- strasbouquet.sql
+++ strasbouquet.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Mar 14 Avril 2020 à 17:21
+-- Généré le :  Mer 06 Mai 2020 à 09:39
 -- Version du serveur :  5.7.29-0ubuntu0.18.04.1
--- Version de PHP :  7.2.24-0ubuntu0.18.04.3
+-- Version de PHP :  7.2.24-0ubuntu0.18.04.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `bouquet` (
   `id` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prix` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `saisonnier` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `prix` float NOT NULL,
+  `description` varchar(255) COLLATE utf8_bin NOT NULL,
+  `saisonnier` varchar(50) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -44,7 +44,7 @@ CREATE TABLE `bouquet_catalogue` (
   `id_bouquet_concept` int(11) NOT NULL,
   `id_catalogue_unitaire` int(11) NOT NULL,
   `quantite` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -56,9 +56,10 @@ CREATE TABLE `bouquet_concept` (
   `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_panier` int(11) NOT NULL,
-  `prix_total` int(11) NOT NULL,
-  `carte` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `prix_total` float DEFAULT NULL,
+  `carte` tinyint(1) DEFAULT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -68,8 +69,9 @@ CREATE TABLE `bouquet_concept` (
 
 CREATE TABLE `bouquet_panier` (
   `id_panier` int(11) NOT NULL,
-  `id_bouquet` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_bouquet` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -79,26 +81,32 @@ CREATE TABLE `bouquet_panier` (
 
 CREATE TABLE `catalogue_unitaire` (
   `id` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
+  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `type` varchar(255) COLLATE utf8_bin NOT NULL,
   `prix` float NOT NULL,
-  `couleur` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `couleur` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `gallerie`
+-- Structure de la table `galerie`
 --
 
-CREATE TABLE `gallerie` (
+CREATE TABLE `galerie` (
   `id` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `file1` varchar(255) NOT NULL,
-  `file2` varchar(255) DEFAULT NULL,
-  `id_bouquet` int(11) NOT NULL,
-  `id_catalogue_unitaire` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `file1` varchar(255) COLLATE utf8_bin NOT NULL,
+  `id_bouquet` int(11) DEFAULT NULL,
+  `id_catalogue_unitaire` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `galerie`
+--
+
+INSERT INTO `galerie` (`id`, `nom`, `file1`, `id_bouquet`, `id_catalogue_unitaire`) VALUES
+(1, 'Boquuet', 'assets/uploads/5eb123d2add8d.png', 6, NULL);
 
 -- --------------------------------------------------------
 
@@ -108,10 +116,11 @@ CREATE TABLE `gallerie` (
 
 CREATE TABLE `panier` (
   `id` int(11) NOT NULL,
-  `id_bouquet` int(11) NOT NULL,
-  `prix_total` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_bouquet` int(11) DEFAULT NULL,
+  `prix_total` float DEFAULT NULL,
+  `id_user` int(11) NOT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -121,14 +130,22 @@ CREATE TABLE `panier` (
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  `num_Tel` int(11) DEFAULT NULL,
-  `role` varchar(255) NOT NULL,
-  `historique` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `firstname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `lastname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `mail` varchar(255) COLLATE utf8_bin NOT NULL,
+  `role` varchar(255) COLLATE utf8_bin NOT NULL,
+  `historique` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `num_Tel` varchar(15) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `firstname`, `lastname`, `password`, `mail`, `role`, `historique`, `num_Tel`) VALUES
+(1, 'Client', 'CLIENT', '$2y$10$MMoyn41UdQUEcRbD9ksbiO0aOv4uM0lwmDLI8TkgYhIe2hKTJguKK', 'client@gmail.com', 'client', NULL, '0203568956'),
+(2, 'Admin', 'SUPER', '$2y$10$i/jG7MGyAvlsGSLaq0NVQ.I7fdYfdcsQhRTu9pOaJfIhd5NLnY062', 'admin@gmail.com', 'admin', NULL, '0356895689');
 
 --
 -- Index pour les tables exportées
@@ -144,23 +161,23 @@ ALTER TABLE `bouquet`
 -- Index pour la table `bouquet_catalogue`
 --
 ALTER TABLE `bouquet_catalogue`
-  ADD KEY `id_bouquet_concept` (`id_bouquet_concept`),
-  ADD KEY `id_catalogue_unitaire` (`id_catalogue_unitaire`);
+  ADD KEY `bouquet_catalogue_ibfk_1` (`id_bouquet_concept`),
+  ADD KEY `bouquet_catalogue_ibfk_2` (`id_catalogue_unitaire`);
 
 --
 -- Index pour la table `bouquet_concept`
 --
 ALTER TABLE `bouquet_concept`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_panier` (`id_panier`);
+  ADD KEY `bouquet_concept_ibfk_1` (`id_user`),
+  ADD KEY `bouquet_concept_ibfk_2` (`id_panier`);
 
 --
 -- Index pour la table `bouquet_panier`
 --
 ALTER TABLE `bouquet_panier`
-  ADD KEY `id_panier` (`id_panier`),
-  ADD KEY `id_bouquet` (`id_bouquet`);
+  ADD KEY `bouquet_panier_ibfk_1` (`id_panier`),
+  ADD KEY `bouquet_panier_ibfk_2` (`id_bouquet`);
 
 --
 -- Index pour la table `catalogue_unitaire`
@@ -169,12 +186,12 @@ ALTER TABLE `catalogue_unitaire`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `gallerie`
+-- Index pour la table `galerie`
 --
-ALTER TABLE `gallerie`
+ALTER TABLE `galerie`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_bouquet` (`id_bouquet`),
-  ADD KEY `id_catalogue_unitaire` (`id_catalogue_unitaire`);
+  ADD KEY `galerie_ibfk_1` (`id_bouquet`),
+  ADD KEY `galerie_ibfk_2` (`id_catalogue_unitaire`);
 
 --
 -- Index pour la table `panier`
@@ -209,10 +226,10 @@ ALTER TABLE `bouquet_concept`
 ALTER TABLE `catalogue_unitaire`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT pour la table `gallerie`
+-- AUTO_INCREMENT pour la table `galerie`
 --
-ALTER TABLE `gallerie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `galerie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `panier`
 --
@@ -222,7 +239,7 @@ ALTER TABLE `panier`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Contraintes pour les tables exportées
 --
@@ -231,35 +248,35 @@ ALTER TABLE `user`
 -- Contraintes pour la table `bouquet_catalogue`
 --
 ALTER TABLE `bouquet_catalogue`
-  ADD CONSTRAINT `bouquet_catalogue_ibfk_1` FOREIGN KEY (`id_bouquet_concept`) REFERENCES `bouquet_concept` (`id`),
-  ADD CONSTRAINT `bouquet_catalogue_ibfk_2` FOREIGN KEY (`id_catalogue_unitaire`) REFERENCES `catalogue_unitaire` (`id`);
+  ADD CONSTRAINT `bouquet_catalogue_ibfk_1` FOREIGN KEY (`id_bouquet_concept`) REFERENCES `bouquet_concept` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bouquet_catalogue_ibfk_2` FOREIGN KEY (`id_catalogue_unitaire`) REFERENCES `catalogue_unitaire` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `bouquet_concept`
 --
 ALTER TABLE `bouquet_concept`
-  ADD CONSTRAINT `bouquet_concept_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `bouquet_concept_ibfk_2` FOREIGN KEY (`id_panier`) REFERENCES `panier` (`id`);
+  ADD CONSTRAINT `bouquet_concept_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bouquet_concept_ibfk_2` FOREIGN KEY (`id_panier`) REFERENCES `panier` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `bouquet_panier`
 --
 ALTER TABLE `bouquet_panier`
-  ADD CONSTRAINT `bouquet_panier_ibfk_1` FOREIGN KEY (`id_panier`) REFERENCES `panier` (`id`),
-  ADD CONSTRAINT `bouquet_panier_ibfk_2` FOREIGN KEY (`id_bouquet`) REFERENCES `bouquet` (`id`);
+  ADD CONSTRAINT `bouquet_panier_ibfk_1` FOREIGN KEY (`id_panier`) REFERENCES `panier` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bouquet_panier_ibfk_2` FOREIGN KEY (`id_bouquet`) REFERENCES `bouquet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `gallerie`
+-- Contraintes pour la table `galerie`
 --
-ALTER TABLE `gallerie`
-  ADD CONSTRAINT `gallerie_ibfk_1` FOREIGN KEY (`id_bouquet`) REFERENCES `bouquet` (`id`),
-  ADD CONSTRAINT `gallerie_ibfk_2` FOREIGN KEY (`id_catalogue_unitaire`) REFERENCES `catalogue_unitaire` (`id`);
+ALTER TABLE `galerie`
+  ADD CONSTRAINT `galerie_ibfk_1` FOREIGN KEY (`id_bouquet`) REFERENCES `bouquet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `galerie_ibfk_2` FOREIGN KEY (`id_catalogue_unitaire`) REFERENCES `catalogue_unitaire` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `panier`
 --
 ALTER TABLE `panier`
-  ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
